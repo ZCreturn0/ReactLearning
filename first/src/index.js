bae4e28f41440b0081bfd96bfbd493639bceef64
ZCreturn0 <237350543@qq.com>
2019-08-08 22:34:16 +0800

    显示下一个玩家

--- first/src/index.js
+++ first/src/index.js
@@ -50,7 +50,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        let status = `next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
         return (
         <div>
