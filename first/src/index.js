546d904ef8bbbe5828e9021aa3233d963d3a0dab
ZCreturn0 <237350543@qq.com>
2019-08-08 22:51:02 +0800

    判断胜负

--- first/src/index.js
+++ first/src/index.js
@@ -13,6 +13,28 @@ import './index.css';
 //         );
 //     }
 // }
+
+// 判断输赢
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 function Square(props){
     return (
         <button
@@ -50,7 +72,14 @@ class Board extends React.Component {
     }
 
     render() {
-        let status = `next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        let winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner){
+            status = 'winner: ' + winner;
+        }
+        else{
+            status = `next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        }
 
         return (
         <div>
