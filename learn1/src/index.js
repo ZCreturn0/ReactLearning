b5d8136b49f81fb3fa3c8946cd5b63e06b08c153
ZCreturn0 <237350543@qq.com>
2019-08-19 23:45:05 +0800

    函数与class定义组件方式

--- learn1/src/index.js
+++ learn1/src/index.js
@@ -8,29 +8,40 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-Date.prototype.Format = function (fmt) {
-    var o = {
-        "M+": this.getMonth() + 1,  // 月份 
-        "d+": this.getDate(),       // 日
-        "h+": this.getHours(),      // 小时
-        "m+": this.getMinutes(),    // 分
-        "s+": this.getSeconds()     // 秒
-    };
-    if (/(y+)/.test(fmt))
-        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt))
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
-}
+// Date.prototype.Format = function (fmt) {
+//     var o = {
+//         "M+": this.getMonth() + 1,  // 月份 
+//         "d+": this.getDate(),       // 日
+//         "h+": this.getHours(),      // 小时
+//         "m+": this.getMinutes(),    // 分
+//         "s+": this.getSeconds()     // 秒
+//     };
+//     if (/(y+)/.test(fmt))
+//         fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+//     for (var k in o)
+//         if (new RegExp("(" + k + ")").test(fmt))
+//             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+//     return fmt;
+// }
 
-function tick(){
-    const timer = <div>
-        <p>time:</p>
-        <p>{new Date().Format('yyyy-MM-dd hh:mm:ss')}</p>
-    </div>;
-    ReactDOM.render(timer, document.getElementById('root'));
+// function tick(){
+//     const timer = <div>
+//         <p>time:</p>
+//         <p>{new Date().Format('yyyy-MM-dd hh:mm:ss')}</p>
+//     </div>;
+//     ReactDOM.render(timer, document.getElementById('root'));
+// }
+// setInterval(() => {
+//     tick();
+// }, 1000);
+
+// 用函数定义组件
+function welcome(props){
+    return <h2>Welcome {props.name}</h2>
 }
-setInterval(() => {
-    tick();
-}, 1000);
+// 用 class 定义组件
+class Hello extends React.Component{
+    render(){
+        return <h2>Hello {this.props.name}</h2>;
+    }
+}
